7c96c3ffee55dda4ed5682800e5202e8d9210c79
Lavanyasugumar96 <lavanyasugumar.slm@gmail.com>
2025-02-26 13:59:28 +0530

    config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { Container } from "@mui/material";
 import { TaskManager } from "./components/TaskManager";
 import { Nav } from "./components/AppBar";
 
+export const config = {
+  endpoint: `https://mern-task-manager-2-b184.onrender.com`,
+};
+
 const App = () => {
   return (
     <>
